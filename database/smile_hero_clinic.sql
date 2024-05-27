cb1549288001613608b9e90e8ede4f6d29af6181
JpVillaruel <100178728+JpVillaruel@users.noreply.github.com>
2024-05-27 18:34:01 +0800

    appointment request
    added email confirmation before displaying appointment on user

--- database/smile_hero_clinic.sql
+++ database/smile_hero_clinic.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2024 at 09:12 PM
+-- Generation Time: May 27, 2024 at 12:31 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `email`, `password`) VALUES
+(1, 'admin@gmail.com', 'admin');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `appointments`
 --
 
@@ -37,6 +56,7 @@ CREATE TABLE `appointments` (
   `date` varchar(30) NOT NULL,
   `time` varchar(30) NOT NULL,
   `location` varchar(30) NOT NULL,
+  `status` varchar(100) NOT NULL,
   `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -44,11 +64,9 @@ CREATE TABLE `appointments` (
 -- Dumping data for table `appointments`
 --
 
-INSERT INTO `appointments` (`user_id`, `appointment_id`, `name`, `email`, `contact`, `message`, `date`, `time`, `location`, `created_at`) VALUES
-('SHCfa50TCU', 'SHC904d', 'leo poldo ', 'leo@gmail.com', '9423142352', 'hi', 'Monday, 05/20/2024', '9:00 AM', 'Main Street, Makati City', '2024-05-14 23:49:50'),
-('SHC8cb8TCU', 'SHCaa42', 'John Paul Dela Cruz', 'delacruz@gmail.com', '9092141212', 'See you', 'Tuesday, 05/21/2024', '1:00 PM', 'Main Street, Makati City', '2024-05-15 03:07:06'),
-('SHC8629TCU', 'SHCe89a', 'kaye paula', 'kayepaula@gmail.com', '9424242424', 'hello', 'Tuesday, 05/21/2024', '11:00 AM', 'Main Street, Makati City', '2024-05-15 01:27:18'),
-('SHCc4cdTCU', 'SHCf37a', 'John Paul', 'jp@gmail.com', '2147483647', 'this is a message', 'Tuesday, 05/21/2024', '12:00 PM', 'Main Street, Makati City', '2024-05-14 23:13:44');
+INSERT INTO `appointments` (`user_id`, `appointment_id`, `name`, `email`, `contact`, `message`, `date`, `time`, `location`, `status`, `created_at`) VALUES
+('SHCf023TCU', 'SHCa31c', 'John Paul Dela Cruz', 'jpvillaruel02@gmail.com', '9070050140', '', 'Friday, 05/31/2024', '4:00 PM', 'Main Street, Makati City', 'accepted', '2024-05-27 18:28:04'),
+('SHC9516TCU', 'SHCf6d7', 'christian jake dela cruz', 'lems.christianjakedelacruz@gmail.com', '9289403281', '', 'Friday, 05/31/2024', '3:00 PM', 'Main Street, Makati City', 'accepted', '2024-05-27 18:21:00');
 
 -- --------------------------------------------------------
 
@@ -72,13 +90,18 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `user_id`, `fullname`, `email`, `contact`, `address`, `pass`, `created_at`) VALUES
-(15, 'SHC61f2TCU', 'Juan Pablo', 'jp@gmail.com', 9141412312, 'Camarines Sur', '$2y$12$.QXA2R4kFrHw6QTsCE0ok.9bjwr0lOvQRZlVwjZnIAgyyk45fBVFa', '2024-05-14 23:44:09'),
-(18, 'SHCfc66TCU', 'christian jake', 'cj@gmail.com', 9341221222, 'Daisy St. South Signal, Taguig City', '$2y$12$e.6wxt5.rtDa0TFELUvoceSpXAiENFzZ./PLz2iXhc45wdU0/ta2i', '2024-05-14 23:47:14'),
-(19, 'SHCfa50TCU', 'leo poldo ', 'leo@gmail.com', 9423142352, NULL, '$2y$12$SqWDz9sKZ1nnE2dCbWwlHuasVyE/J2lXX/wNW6y1d8B2PcR2uXChy', '2024-05-14 23:47:57');
+(31, 'SHC9516TCU', 'christian jake dela cruz', 'lems.christianjakedelacruz@gmail.com', 9289403281, NULL, '$2y$12$zcDf4JbV2m4mFa.vQrD2JOlr1XL11pxl2JG9MvVR/VQyf8KbfHcg2', '2024-05-27 18:20:47'),
+(32, 'SHCf023TCU', 'John Paul Dela Cruz', 'jpvillaruel02@gmail.com', 9070050140, NULL, '$2y$12$Vk/hlRGNM7G70IwXPxGxXuGQecd9bhvDUts5tWtZi7PFXGgalHq32', '2024-05-27 18:22:07');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `appointments`
@@ -97,10 +120,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
