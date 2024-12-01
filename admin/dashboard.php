62e5b90406bf1ca45853ba60e72cc4ad99ef2dbd
Fahatmah <fahatmahmabang9@gmail.com>
2024-12-01 08:34:15 +0800

    feat(admin): add dropdown for on duty dentist of today

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -193,60 +193,56 @@ $startOfMonth = date('Y-m-01');
                 </svg>
               </button>
 
-              <div class="appointment-summary" id="appointmentSummary">
-                <p class="summary-title">Number of appointments</p>
+              <div class="appointment-summary dropdown" id="dropdown">
+                <p class="summary-title title">Number of appointments</p>
                 <hr>
-                <div class="appointment-types">
-                  <p class="appointment-type">Walk-ins<span class="appointment-count"><?php echo $walkInCount; ?></span></p>
-                  <p class="appointment-type">Online<span class="appointment-count"><?php echo $onlineCount; ?></span></p>
+                <div class="appointment-types lists">
+                  <p class="appointment-type list">Walk-ins<span class="appointment-count"><?php echo $walkInCount; ?></span></p>
+                  <p class="appointment-type list">Online<span class="appointment-count"><?php echo $onlineCount; ?></span></p>
                 </div>
               </div>
             </div>
              <div class="overview__icon-wrapper">
-               <svg width="36" height="36" viewBox="0 0 36 36"      fill="none" xmlns="http://www.w3.org/2000/svg"    aria-label="Today's Schedule Icon">
-                 <rect width="36" height="36" rx="18"     fill="#1D72F2" />
-                 <path d="M14 8V11" stroke="white" stroke-width="1.     5" stroke-linecap="round" stroke-linejoin="round"/>
-                 <path d="M22 8V11" stroke="white" stroke-width="1.     5" stroke-linecap="round" stroke-linejoin="round"/>
-                 <path d="M9.5 15.0901H26.5" stroke="white"     stroke-width="1.5" stroke-linecap="round"     stroke-linejoin="round"/>
-                 <path d="M27 14.5V23C27 26 25.5 28 22 28H14C10.5 28      9 26 9 23V14.5C9 11.5 10.5 9.5 14 9.5H22C25.5 9.5     27 11.5 27 14.5Z" stroke="white" stroke-width="1.5"      stroke-linecap="round" stroke-linejoin="round"/>
-                 <path d="M21.6947 19.7H21.7037" stroke="white"     stroke-width="2" stroke-linecap="round"     stroke-linejoin="round"/>
-                 <path d="M21.6947 22.7H21.7037" stroke="white"     stroke-width="2" stroke-linecap="round"     stroke-linejoin="round"/>
-                 <path d="M17.9955 19.7H18.0045" stroke="white"     stroke-width="2" stroke-linecap="round"     stroke-linejoin="round"/>
-                 <path d="M17.9955 22.7H18.0045" stroke="white"     stroke-width="2" stroke-linecap="round"     stroke-linejoin="round"/>
-                 <path d="M14.2943 19.7H14.3033" stroke="white"     stroke-width="2" stroke-linecap="round"     stroke-linejoin="round"/>
-                 <path d="M14.2943 22.7H14.3033" stroke="white"     stroke-width="2" stroke-linecap="round"     stroke-linejoin="round"/>
-               </svg>
-               <p class="overview__value" id="todaysSchedule"><?php echo $todaysAppointment; ?></p>
+              <img src="../assets/admin_assets/todays-schedule.svg" alt="">
+              <p class="overview__value" id="todaysSchedule"><?php echo $todaysAppointment; ?></p>
              </div>
            </section>
 
            <section class="overview__item">
              <p class="overview__label">Total Patients</p>
              <div class="overview__icon-wrapper">
-               <svg width="37" height="36" viewBox="0 0 37 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect x="0.666656" width="36" height="36" rx="18" fill="#E84531"/>
-              <path d="M24.6667 13.16C24.6067 13.15 24.5367 13.15 24.4767 13.16C23.0967 13.11 21.9967 11.98 21.9967 10.58C21.9967 9.15 23.1467 8 24.5767 8C26.0067 8 27.1567 9.16 27.1567 10.58C27.1467 11.98 26.0467 13.11 24.6667 13.16Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-              <path d="M23.6367 20.4399C25.0067 20.6699 26.5167 20.4299 27.5767 19.7199C28.9867 18.7799 28.9867 17.2399 27.5767 16.2999C26.5067 15.5899 24.9767 15.3499 23.6067 15.5899" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-              <path d="M12.6367 13.16C12.6967 13.15 12.7667 13.15 12.8267 13.16C14.2067 13.11 15.3067 11.98 15.3067 10.58C15.3067 9.15 14.1567 8 12.7267 8C11.2967 8 10.1467 9.16 10.1467 10.58C10.1567 11.98 11.2567 13.11 12.6367 13.16Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-              <path d="M13.6667 20.4399C12.2967 20.6699 10.7867 20.4299 9.72666 19.7199C8.31666 18.7799 8.31666 17.2399 9.72666 16.2999C10.7967 15.5899 12.3267 15.3499 13.6967 15.5899" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-              <path d="M18.6667 20.63C18.6067 20.62 18.5367 20.62 18.4767 20.63C17.0967 20.58 15.9967 19.45 15.9967 18.05C15.9967 16.62 17.1467 15.47 18.5767 15.47C20.0067 15.47 21.1567 16.63 21.1567 18.05C21.1467 19.45 20.0467 20.59 18.6667 20.63Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-              <path d="M15.7567 23.7799C14.3467 24.7199 14.3467 26.2599 15.7567 27.1999C17.3567 28.2699 19.9767 28.2699 21.5767 27.1999C22.9867 26.2599 22.9867 24.7199 21.5767 23.7799C19.9867 22.7199 17.3567 22.7199 15.7567 23.7799Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-              </svg>
+               <img src="../assets/admin_assets/new-patients.svg" alt="">
 
                <p class="overview__value" id="newPatients"><?php echo $totalUsers; ?></p>
              </div>
            </section>
 
            <section class="overview__item">
-             <p class="overview__label">Doctor's On-Duty</p>
-             <div class="overview__icon-wrapper">
-               <svg width="31" height="32" viewBox="0 0 31 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect x="0.333374" width="30" height="32" rx="15" fill="#FAAF0D"/>
-              <path d="M13.3334 10H17.3334C19.3334 10 19.3334 9 19.3334 8C19.3334 6 18.3334 6 17.3334 6H13.3334C12.3334 6 11.3334 6 11.3334 8C11.3334 10 12.3334 10 13.3334 10Z" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-              <path d="M19.3334 10.0348C22.6634 10.2205 24.3334 8.91066 24.3334 13.6239V19.8119C24.3334 23.9373 23.3334 26 18.3334 26H12.3334C7.33337 26 6.33337 23.9373 6.33337 19.8119V13.6239C6.33337 8.92097 8.00337 10.2205 11.3334 10.0348" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-              <path d="M18.3334 18H12.3334" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-              <path d="M15.3334 21V15" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+            <div class="header">
+              <p class="overview__label">Doctor's On-Duty</p>
+              <button class="more-btn" id="moreBtn">
+                <svg width="25" height="12" viewBox="0 0 25 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="6.66666" cy="6" r="2" fill="#616161"/>
+                <circle cx="12.6667" cy="6" r="2" fill="#616161"/>
+                <circle cx="18.6667" cy="6" r="2" fill="#616161"/>
               </svg>
+              </button>
+
+              <div class="doctors-on-duty dropdown" id="dropdown">
+                <p class="header-title title">On Duty Doctors <span>6</span></p>
+                <hr>
+                <ul class="doctors lists">
+                  <li class="list">Doctor Name</li>
+                  <li class="list">Doctor Name</li>
+                  <li class="list">Doctor Name</li>
+                  <li class="list">Doctor Name</li>
+                  <li class="list">Doctor Name</li>
+                  <li class="list">Doctor Name</li>
+                </ul>
+              </div>
+            </div>
+             <div class="overview__icon-wrapper">
+               <img src="../assets/admin_assets/doctors-on-duty.svg" alt="">
 
                <p class="overview__value" id="doctorsOnDuty"><?php echo $doctors ?></p>
              </div>
@@ -514,11 +510,14 @@ $startOfMonth = date('Y-m-01');
     .catch(error => console.error('Error fetching monthly appointments data:', error));
 
     // overview pop up
-    const summary = document.getElementById('appointmentSummary')
-    const moreBtn = document.getElementById('moreBtn')
+    // const dropdown = document.querySelectorAll('#dropdown')
+    const moreBtns = document.querySelectorAll('#moreBtn')
 
-    moreBtn.addEventListener('click', (e) => {
-      summary.classList.toggle('active')
+    moreBtns.forEach(btn => {
+      btn.addEventListener('click', (e) => {
+        const dropdown = e.currentTarget.nextElementSibling        
+        dropdown.classList.toggle('active')
+      })
     })
 
     const filterSummary = document.getElementById('filterSummary')
