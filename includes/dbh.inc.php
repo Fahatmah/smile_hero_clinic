f87442045795af1a12813a55af905889f8f409a3
JpVillaruel <100178728+JpVillaruel@users.noreply.github.com>
2024-12-14 11:12:08 +0800

    fix: UTC accuracy for database server's time zone

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,13 +1,16 @@
 <?php
 
+date_default_timezone_set('Asia/Manila');
+
 $host = 'localhost';
 $dbname = 'smile_hero_clinic';
 $dbusername = 'root';
 $dbpassword = '';
 
-
 $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
 
+// Set timezone for MySQL
+$conn->query("SET time_zone = '+08:00'");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
